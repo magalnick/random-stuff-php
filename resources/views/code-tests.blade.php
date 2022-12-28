40984a849c1c49976a406079a19b1fbb34623ef5
David Magalnick <david@phone.com>
2022-12-27 16:33:20 -0800

    Refactor code tests to be not the home page, add the Lean TECHniques Photo Album link

--- resources/views/code-tests.blade.php
+++ resources/views/code-tests.blade.php
@@ -1,6 +1,9 @@
 <div class="row">
     <div class="col">
         <div>
+            <a href="/leantechniques-photo-album">Lean TECHniques Photo Album</a>
+        </div>
+        <div>
             <a href="/clinicomp-code-test">CliniComp Code Test</a>
         </div>
         <div>
