e727bcd8a84d564d192b7b451fa91e2bf8709be2
David Magalnick <david@phone.com>
2022-12-27 16:32:00 -0800

    Refactor code tests to be not the home page

---
+++ resources/views/code-tests.blade.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col">
+        <div>
+            <a href="/clinicomp-code-test">CliniComp Code Test</a>
+        </div>
+        <div>
+            <a href="/mailchimp-markdown-test">Mailchimp Markdown Test</a>
+        </div>
+    </div>
+</div>
